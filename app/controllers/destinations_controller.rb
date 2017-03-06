0abb224c6541fef59d33be6bd74e78d65068bb58
Steve Ernstberger <sernstberger@developertown.com>
2017-03-05 22:23:20 -0500

    Improved contrast and ordered destinations

--- app/controllers/destinations_controller.rb
+++ app/controllers/destinations_controller.rb
@@ -2,7 +2,7 @@ class DestinationsController < ApplicationController
   before_action :set_destination, only: [:show, :edit, :update, :destroy]
 
   def index
-    @destinations = Destination.all
+    @destinations = Destination.order('name ASC')
   end
 
   def show
--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -2,7 +2,7 @@ class HomeController < ApplicationController
 
   def index
     @attractions = Attraction.all
-    @destinations = Destination.all
+    @destinations = Destination.order('name ASC').first(3)
     @posts = Post.all
   end
 
