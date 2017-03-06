6b7a16217f9648b3b5d569accf39c5401360ee9e
Steve Ernstberger <sernstberger@developertown.com>
2017-03-06 13:41:31 -0500

    Removed a bunch of unused crud

--- app/controllers/admin/photos_controller.rb
+++ app/controllers/admin/photos_controller.rb
@@ -19,14 +19,10 @@ class Admin::PhotosController < Admin::BaseController
   def create
     @photo = Photo.new(photo_params)
 
-    respond_to do |format|
-      if @photo.save
-        format.html { redirect_to @photo, notice: 'Photo was successfully created.' }
-        format.json { render :show, status: :created, location: @photo }
-      else
-        format.html { render :new }
-        format.json { render json: @photo.errors, status: :unprocessable_entity }
-      end
+    if @photo.save
+      redirect_to admin_photos_path, notice: 'Photo was successfully created.'
+    else
+      render :new
     end
   end
 
