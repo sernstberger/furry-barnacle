ae72abef7d7dec872e76ab897120c64d35aeb424
Steve Ernstberger <sernstberger@developertown.com>
2017-03-06 23:58:22 -0500

    Styled it up. Lookin good.

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -4,6 +4,7 @@ class HomeController < ApplicationController
     @attractions = Attraction.all
     @destinations = Destination.order('name ASC').first(3)
     @posts = Post.all
+    @featured_post = Post.where(featured: true).first
   end
 
 end
